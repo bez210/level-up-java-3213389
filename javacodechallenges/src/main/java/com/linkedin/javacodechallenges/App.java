2847f20e7e153829721385e430fd9f931237d30e
bez210 <119533825+bez210@users.noreply.github.com>
2022-11-30 19:53:41 +0000

    chap1.2

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if (password.length() < 6) {
+            return false;
+        }
+
+        boolean hasLowerCaseLetter = false;
+        boolean hasUpperCaseLetter = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i < password.length() || !hasLowerCaseLetter && !hasUpperCaseLetter && !hasNumber; i++) {
+            char current = password.charAt(i);
+            if (Character.isDigit(current)) {
+                hasNumber = true;
+            } else if (Character.isUpperCase(current)) {
+                hasUpperCaseLetter = true;
+            } else if (Character.isLowerCase(current)) {
+                hasLowerCaseLetter = true;
+            }
+
+        }
+
+        return hasNumber && hasLowerCaseLetter && hasUpperCaseLetter;
     }
 
     public static void main(String[] args) {
